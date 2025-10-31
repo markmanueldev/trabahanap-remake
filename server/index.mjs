3ec28ea6529c021ec7f3080974bb188908719586
markmanueldev <domaumaru5@gmail.com>
2025-10-31 19:15:25 +0800

    modified to not run the server when testing

--- server/index.mjs
+++ server/index.mjs
@@ -1,7 +1,8 @@
 import express from "express";
 import winston from "winston";
 import { connectDB } from "./connection/mongodb_connection.mjs";
-import employerProfiles from "./routes/employer_routes/employer_profiles.mjs";
+import employerProfiles from "./routes/employer_routes/employer_profile.mjs";
+import jobListings from "./routes/employer_routes/job_listing.mjs";
 import jobSeekerProfiles from "./routes/job_seeker_routes/job_seeker_profiles.mjs";
 
 const logger = winston.createLogger({
@@ -15,19 +16,23 @@ app.use(express.json());
 //defined routes
 app.use("/api/employers", employerProfiles);
 app.use("/api/job_seekers", jobSeekerProfiles);
+app.use("/api/job_listings", jobListings);
 
 async function startServer() {
   try {
     await connectDB();
-    app.listen(3000, () => {
+    const server = app.listen(3000, () => {
       logger.info("Server is running on port 3000");
     });
+    return server;
   } catch (err) {
     console.error("Startup failed:", err);
     process.exit(1);
   }
 }
 
-startServer();
+if (import.meta.url === `file:///${process.argv[1].replace(/\\/g, '/')}`) {
+  startServer();
+}
 
 export default app;
